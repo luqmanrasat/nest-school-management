db4402660340941884ebba4dc213c766f7664b13
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-02 20:03:37 +0800

    add create lesson service

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class LessonService {
@@ -9,4 +10,15 @@ export class LessonService {
     @InjectRepository(Lesson)
     private lessonRepository: Repository<Lesson>,
   ) {}
+
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
 }
